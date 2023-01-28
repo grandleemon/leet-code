564a9542b9b71c0c009b3d423191306379310e8f
Max <zeetreex@gmail.com>
2023-01-28 12:09:02 +0200

    9. Palindrome Number

---
+++ Easy/9. Palindrome Number.js
@@ -0,0 +1,42 @@
+// 9. Palindrome Number
+// Easy
+// 8.5K
+// 2.4K
+// Companies
+// Given an integer x, return true if x is a
+// palindrome
+//     , and false otherwise.
+//
+//
+//
+//     Example 1:
+//
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+//     Example 2:
+//
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+//     Example 3:
+//
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+//
+//
+//     Constraints:
+//
+// -231 <= x <= 231 - 1
+//
+//
+// Follow up: Could you solve it without converting the integer to a string?
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    return x.toString().split("").reverse().join("") === x.toString()
+};
