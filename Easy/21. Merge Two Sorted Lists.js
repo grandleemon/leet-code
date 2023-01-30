7ff8cbfe8991ede81342166c80eb9c184fd99661
Max <zeetreex@gmail.com>
2023-01-30 19:25:50 +0200

    21. Merge Two Sorted Lists explanation

--- Easy/21. Merge Two Sorted Lists.js
+++ Easy/21. Merge Two Sorted Lists.js
@@ -57,3 +57,6 @@ var mergeTwoLists = function(list1, list2) {
         return list2
     }
 };
+
+// Explanation
+// To solve this problem, we can use recursion, calling it until one of the sheets is null, and while it is not null, we do an equality check, if list1 is less than or equal to list2, we call recursion with list1.next = mergeTwoLists (list1.next, list2), else if the value is greater than list2.next = mergeTwoLists(list1, list2.next). If one of the sheets is null, we simply return the opposite list (we don't care if it's null or not). Thus, when the recursion ends, the stack will begin to collapse in reverse order, which will allow us to get a new merged sorted list.
