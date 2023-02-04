2971d10ae7e74676d22067a3d923a5ca67b48bd8
Max <zeetreex@gmail.com>
2023-02-04 13:07:53 +0200

    1. Two Sum optimization

--- Easy/1. Two Sum.js
+++ Easy/1. Two Sum.js
@@ -41,16 +41,28 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//     const res = []
+//
+//     for(let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] === target && res.length !== 2) {
+//                 res.push(i, j)
+//             }
+//         }
+//     }
+//
+//     return res
+// };
+
 var twoSum = function(nums, target) {
-    const res = []
+    const hashMap = new Map()
 
     for(let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target && res.length !== 2 && i !== j) {
-                res.push(i, j)
-            }
-        }
+        let diff = target - nums[i]
+        if(hashMap.has(diff)) return [i, hashMap.get(diff)]
+        hashMap.set(nums[i], i)
     }
-
-    return res
 };
+
+console.log(twoSum([2,7,11,15], 9))
